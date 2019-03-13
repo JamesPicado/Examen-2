e15748de08ba7577e4500da954826d34f545b530
JamesPicado <picadojames.jp@gmail.com>
2019-03-13 16:09:05 -0600

    actualizacion

--- app/src/main/java/net/unadeca/testdatabase/database/models/Arbolito.java
+++ app/src/main/java/net/unadeca/testdatabase/database/models/Arbolito.java
@@ -2,10 +2,14 @@ package net.unadeca.testdatabase.database.models;
 
 import com.raizlabs.android.dbflow.annotation.Column;
 import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
 import com.raizlabs.android.dbflow.structure.BaseModel;
+
+import net.unadeca.testdatabase.database.TestDatabase;
 
 import java.util.Locale;
 
+@Table(database = TestDatabase.class)
 public class Arbolito extends BaseModel {
 
     @Column
